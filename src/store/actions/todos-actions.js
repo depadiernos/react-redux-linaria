5833464ebfa30580b5aca66ec76c79dc82dbfe7a
Dennis Padiernos <depadiernos@gmail.com>
2020-02-25 13:23:47 -0600

    scaffold redux

---
+++ src/store/actions/todos-actions.js
@@ -0,0 +1,26 @@
+// CONSTANTS
+
+// action types
+export const ADD_TODO = "ADD_TODO"
+export const TOGGLE_TODO = "TOGGLE_TODO"
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER"
+
+// filter types
+export const VisibilityFilders = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+}
+
+// ACTION CREATORS
+export const addTodo = (text) => {
+  return { type: ADD_TODO, text }
+}
+
+export const toggleTodo = (index) => {
+  return { type: TOGGLE_TODO, index }
+}
+
+export const setVisibilityFilter = (filter) => {
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
---
+++ src/store/reducers/todos-reducer.js
@@ -0,0 +1,31 @@
+import { combineReducers } from "redux"
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from "../actions/todos-actions"
+
+const todo = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, completed: false }]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        index === action.index ? { ...todo, completed: !todo.completed } : todo
+      })
+    default:
+      return state
+  }
+}
+
+const visibilityFilter = (state = SHOW_ALL, action) => {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default:
+      return state
+  }
+}
+
+const todos = combineReducers({
+  visibilityFilter,
+  todo
+})
+
+export default todos
